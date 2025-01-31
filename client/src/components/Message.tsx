899e7e7bf3f6924ae1dc37c68cbb33b2a45e2f97
HariomRajChauhan <chauhanhariomraj7@gmail.com>
2025-01-31 20:12:39 +0545

    client

---
+++ client/src/components/Message.tsx
@@ -0,0 +1,15 @@
+
+
+function Message()
+{
+    return (
+        <div>
+            <h2>Name :</h2><h3>Hari</h3>
+            <h2>Message :</h2><h3>Hi</h3>
+            <h2>Time :</h2><h3>10:00</h3>
+        </div>
+    );
+
+}
+
+export default Message;
