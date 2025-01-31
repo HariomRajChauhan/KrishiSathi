fd17dbf71dbc68cee8b770393f4be7681f10fcab
HariomRajChauhan <chauhanhariomraj7@gmail.com>
2025-01-31 20:19:45 +0545

    client added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,12 @@
-import Message from './components/Message';
 import NavBar from './components/NavBar';
-import Hero from './components/Hero';
-import Login from './components/Login';
+
 
 
 function App()
 {
   return (
     <div className='h-screen w-screen bg-gray-100 p-2 '>
-      <NavBar  />
-      <Hero />
-      <Login />
-
-      <h1>Chat App</h1>
-      <Message />
+      <NavBar/>
     </div>
   );
 }
